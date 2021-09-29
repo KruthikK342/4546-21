5a56a124b51ed3a0258d0a8b6b70f369cc72de3a
cecilia512 <ceciliacskinner05@gmail.com>
2021-09-29 15:29:00 -0500

    Update Teleop.java

--- TeamCode/src/main/java/Opmode/Teleop.java
+++ TeamCode/src/main/java/Opmode/Teleop.java
@@ -33,6 +33,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -46,6 +47,11 @@ public class Teleop extends LinearOpMode {
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
     private DcMotor intake = null;
+    private DcMotor outake = null;
+    private Servo sort = null;
+    private DcMotor spin = null;
+    private DcMotor arm = null;
+    private Servo wrist = null;
 
     @Override
     public void runOpMode() {
@@ -53,13 +59,8 @@ public class Teleop extends LinearOpMode {
         telemetry.update();
 
         // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
@@ -67,39 +68,34 @@ public class Teleop extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
+        // starting program
         while (opModeIsActive()) {
 
-            // Setup a variable for each drive wheel to save power level for telemetry
+            // Driving [arcade mode]
             double leftPower;
             double rightPower;
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
             double drive = -gamepad1.left_stick_y;
             double turn  =  gamepad1.right_stick_x;
             leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
             rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
-
-            // Send calculated power to wheels
             leftDrive.setPower(leftPower);
             rightDrive.setPower(rightPower);
 
-            if(gamepad1.right_trigger > .5) {
-                intake.setPower(-1);
-            }
+            // Driver1 controls- intake, outake, sorting, driving
+            if(gamepad1.right_trigger > .5) intake.setPower(-1);
+            if(gamepad1.left_trigger > .5) intake.setPower(1);
+            if(gamepad1.left_bumper) outake.setPower(.7);
+            if (gamepad1.x) sort.setPosition(.5);
+            if (gamepad1.y) sort.setPosition(0);
 
-            if(gamepad1.left_trigger > .5) {
-                intake.setPower(1);
-            }
+            // Driver2 controls- carousel, pulley, wrist motion
+            if (gamepad2.right_bumper) spin.setPower(.2);
+            if (gamepad2.right_trigger > .5) arm.setPower(.2);
+            if (gamepad2.right_trigger < .5) arm.setPower(0);
+            if (gamepad2.y) wrist.setPosition(0);
+            if (gamepad2.x) wrist.setPosition(1);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
