f7c4d0d222ded05f9446babf98ebefb0f31ba575
cecilia512 <ceciliacskinner05@gmail.com>
2021-09-29 15:37:35 -0500

    Update Teleop.java

--- TeamCode/src/main/java/Opmode/Teleop.java
+++ TeamCode/src/main/java/Opmode/Teleop.java
@@ -44,14 +44,14 @@ public class Teleop extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
-    private DcMotor intake = null;
-    private DcMotor outake = null;
-    private Servo sort = null;
-    private DcMotor spin = null;
-    private DcMotor arm = null;
-    private Servo wrist = null;
+    private DcMotor leftDrive = null; // left drive motor
+    private DcMotor rightDrive = null; // right drive motor
+    private DcMotor intake = null; // intake motor
+    private DcMotor outake = null; // outputting unneccesary
+    private Servo sort = null; // sorting items collected
+    private DcMotor spin = null; // carousel control motor
+    private DcMotor arm = null; // pulley motor
+    private Servo wrist = null; // wrist like outake
 
     @Override
     public void runOpMode() {
@@ -75,27 +75,27 @@ public class Teleop extends LinearOpMode {
             double leftPower;
             double rightPower;
 
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
+            double drive = -gamepad1.left_stick_y; // forward and backwards
+            double turn  =  gamepad1.right_stick_x; // left and right
             leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
             rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
             leftDrive.setPower(leftPower);
             rightDrive.setPower(rightPower);
 
-            // Driver1 controls- intake, outake, sorting, driving
-            if(gamepad1.right_trigger > .5) intake.setPower(-1);
-            if(gamepad1.left_trigger > .5) intake.setPower(1);
-            if(gamepad1.left_bumper) outake.setPower(.7);
-            if (gamepad1.x) sort.setPosition(.5);
-            if (gamepad1.y) sort.setPosition(0);
+            //Driver 1
+            if(gamepad1.right_trigger > .5) intake.setPower(-1); // Intake out
+            if(gamepad1.left_trigger > .5) intake.setPower(1); //Intake in
+            if(gamepad1.left_bumper) outake.setPower(.7); //Outake
+            if (gamepad1.x) sort.setPosition(.5); //sorting one direction
+            if (gamepad1.y) sort.setPosition(0); //sorting other direction
 
-            // Driver2 controls- carousel, pulley, wrist motion
-            if (gamepad2.right_bumper) spin.setPower(.2);
-            if (gamepad2.right_trigger > .5) arm.setPower(.2);
-            if (gamepad2.right_trigger < .5) arm.setPower(0);
-            if (gamepad2.y) wrist.setPosition(0);
-            if (gamepad2.x) wrist.setPosition(1);
+            // Driver 2
+            if (gamepad2.right_bumper) spin.setPower(.2); // Carousel
+            if (gamepad2.right_trigger > .5) arm.setPower(.2); // Pulley up
+            if (gamepad2.right_trigger < .5) arm.setPower(0); // Pulley stop
+            if (gamepad2.y) wrist.setPosition(0); // Wrist out
+            if (gamepad2.x) wrist.setPosition(1); // Wrist in
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
