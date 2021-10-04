b48a48b9e436635cb758c85f6a687357468add0b
Kruthik <kruthik.p.kalidas@gmail.com>
2021-10-04 17:30:25 -0500

    Update VuforiaBitmap

--- TeamCode/src/main/java/Library/VuforiaBitmap.java
+++ TeamCode/src/main/java/Library/VuforiaBitmap.java
@@ -29,18 +29,7 @@ public class VuforiaBitmap extends LinearOpMode
 
     final double MM_PER_INCH = 25.40 ;   //  Metric conversion
 
-    /*
-     * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
-     * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
-     * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
-     * web site at https://developer.vuforia.com/license-manager.
-     *
-     * Vuforia license keys are always 380 characters long, and look as if they contain mostly
-     * random data. As an example, here is a example of a fragment of a valid key:
-     *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
-     * Once you've obtained a license key, copy the string from the Vuforia web site
-     * and paste it in to your code on the next line, between the double quotes.
-     */
+
     private static final String VUFORIA_KEY =
             " --- YOUR NEW VUFORIA KEY GOES HERE  --- ";
 
